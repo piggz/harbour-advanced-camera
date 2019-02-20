b588ec0986f3aca212616aaf84966454600d23d3
Vincent Knecht <vincent.knecht@mailoo.org>
2019-02-20 18:38:54 +0100

    [l10n] Add french translation

---
+++ translations/harbour-advanced-camera-fr.ts
@@ -0,0 +1,362 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="8"/>
+        <source>My Cover</source>
+        <translation>Advanced Camera</translation>
+    </message>
+</context>
+<context>
+    <name>EffectsModel</name>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="64"/>
+        <source>None</source>
+        <translation>Aucun</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="67"/>
+        <source>Aqua</source>
+        <translation>Eau</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="70"/>
+        <source>Blackboard</source>
+        <translation>Tableau noir</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="73"/>
+        <source>Grayscale</source>
+        <translation>Gris</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="76"/>
+        <source>Negative</source>
+        <translation>Négatif</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="79"/>
+        <source>Posterize</source>
+        <translation>Postériser</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="82"/>
+        <source>Sepia</source>
+        <translation>Sepia</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="85"/>
+        <source>Solarize</source>
+        <translation>Solariser</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="88"/>
+        <source>Whiteboard</source>
+        <translation>Tableau blanc</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="91"/>
+        <source>Emboss</source>
+        <translation>Relief</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="94"/>
+        <source>Sketch</source>
+        <translation>Esquisse</translation>
+    </message>
+    <message>
+        <location filename="../src/effectsmodel.cpp" line="97"/>
+        <source>Neon</source>
+        <translation>Néon</translation>
+    </message>
+</context>
+<context>
+    <name>ExposureModel</name>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="64"/>
+        <source>Automatic Scene Detection</source>
+        <translation>Automatique</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="67"/>
+        <source>Action</source>
+        <translation>Action</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="70"/>
+        <source>Off</source>
+        <translation>Désactivé</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="73"/>
+        <source>Augmented Reality</source>
+        <translation>Réalité augmentée</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="76"/>
+        <source>Backlight</source>
+        <translation>Contre-jour</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="79"/>
+        <source>Barcode</source>
+        <translation>Code-barres</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="82"/>
+        <source>Beach</source>
+        <translation>Plage</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="85"/>
+        <source>Candlelight</source>
+        <translation>Bougie</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="88"/>
+        <source>Fireworks</source>
+        <translation>Feu d&apos;artifice</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="91"/>
+        <source>Flowers</source>
+        <translation>Fleurs</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="94"/>
+        <source>HDR</source>
+        <translation>HDR</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="97"/>
+        <source>Landscape</source>
+        <translation>Paysage</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="100"/>
+        <source>Large Aperture</source>
+        <translation>Grande ouverture</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="106"/>
+        <source>Night</source>
+        <translation>Nuit</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="109"/>
+        <source>Night Portrait</source>
+        <translation>Portrait de nuit</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="112"/>
+        <source>Party</source>
+        <translation>Fête</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="115"/>
+        <source>Portrait</source>
+        <translation>Portrait</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="118"/>
+        <source>Small Aperture</source>
+        <translation>Petite ouverture</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="121"/>
+        <source>Snow</source>
+        <translation>Neige</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="124"/>
+        <source>Sports</source>
+        <translation>Sports</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="127"/>
+        <source>Spotlight</source>
+        <translation>Projecteur</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="130"/>
+        <source>Steady Photo</source>
+        <translation>Photo stable</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="133"/>
+        <source>Sunset</source>
+        <translation>Coucher de soleil</translation>
+    </message>
+    <message>
+        <location filename="../src/exposuremodel.cpp" line="136"/>
+        <source>Theatre</source>
+        <translation>Théâtre</translation>
+    </message>
+</context>
+<context>
+    <name>FlashModel</name>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="48"/>
+        <location filename="../src/flashmodel.cpp" line="81"/>
+        <source>Unknown</source>
+        <translation>Inconnu</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="61"/>
+        <source>Auto</source>
+        <translation>Automatique</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="63"/>
+        <source>Off</source>
+        <translation>Désactivé</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="65"/>
+        <source>On</source>
+        <translation>Activé</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="67"/>
+        <source>Red Eye Reduction</source>
+        <translation>Réduction des yeux rouges</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="69"/>
+        <source>Fill</source>
+        <translation>Remplissage</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="71"/>
+        <source>Torch</source>
+        <translation>Torche</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="73"/>
+        <source>Video Light</source>
+        <translation>Torche vidéo</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="75"/>
+        <source>Slow Sync Front Curtain</source>
+        <translation>Sync. 1er rideau</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="77"/>
+        <source>Slow Sync Rear Curtain</source>
+        <translation>Sync. 2ème rideau</translation>
+    </message>
+    <message>
+        <location filename="../src/flashmodel.cpp" line="79"/>
+        <source>Manual</source>
+        <translation>Manuel</translation>
+    </message>
+</context>
+<context>
+    <name>FocusModel</name>
+    <message>
+        <location filename="../src/focusmodel.cpp" line="48"/>
+        <location filename="../src/focusmodel.cpp" line="73"/>
+        <source>Unknown</source>
+        <translation>Inconnu</translation>
+    </message>
+    <message>
+        <location filename="../src/focusmodel.cpp" line="61"/>
+        <source>Manual</source>
+        <translation>Manuel</translation>
+    </message>
+    <message>
+        <location filename="../src/focusmodel.cpp" line="63"/>
+        <source>Hyperfocal</source>
+        <translation>Hyperfocale</translation>
+    </message>
+    <message>
+        <location filename="../src/focusmodel.cpp" line="65"/>
+        <source>Infinity</source>
+        <translation>Infini</translation>
+    </message>
+    <message>
+        <location filename="../src/focusmodel.cpp" line="67"/>
+        <source>Auto</source>
+        <translation>Automatique</translation>
+    </message>
+    <message>
+        <location filename="../src/focusmodel.cpp" line="69"/>
+        <source>Continuous</source>
+        <translation>Continu</translation>
+    </message>
+    <message>
+        <location filename="../src/focusmodel.cpp" line="71"/>
+        <source>Macro</source>
+        <translation>Macro</translation>
+    </message>
+</context>
+<context>
+    <name>IsoModel</name>
+    <message>
+        <location filename="../src/isomodel.cpp" line="62"/>
+        <source>Auto ISO</source>
+        <translation>ISO automatique</translation>
+    </message>
+</context>
+<context>
+    <name>WbModel</name>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="63"/>
+        <source>Auto</source>
+        <translation>Automatique</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="65"/>
+        <source>Manual</source>
+        <translation>Manuel</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="67"/>
+        <source>Sunlight</source>
+        <translation>Lumière du jour</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="69"/>
+        <source>Cloudy</source>
+        <translation>Nuageux</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="71"/>
+        <source>Shade</source>
+        <translation>Ombre</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="73"/>
+        <source>Tungsten</source>
+        <translation>Tungstène</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="75"/>
+        <source>Fluorescent</source>
+        <translation>Fluorescent</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="77"/>
+        <source>Flash</source>
+        <translation>Flash</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="79"/>
+        <source>Sunset</source>
+        <translation>Coucher de soleil</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="81"/>
+        <source>Warm Fluorescent</source>
+        <translation>Fluorescent chaud</translation>
+    </message>
+    <message>
+        <location filename="../src/wbmodel.cpp" line="83"/>
+        <source>Unknown</source>
+        <translation>Inconnu</translation>
+    </message>
+</context>
+</TS>
