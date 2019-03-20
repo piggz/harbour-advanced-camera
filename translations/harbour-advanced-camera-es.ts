4ecc61c8525b53deb63037e67bf7581d56722344
Carmen F. B <carmenfdezb@mail.com>
2019-03-20 21:11:21 +0100

    Added Spanish language

---
+++ translations/harbour-advanced-camera-es.ts
@@ -0,0 +1,300 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Advanced Camera</source>
+        <translation>Cámara avanzada</translation>
+    </message>
+</context>
+<context>
+    <name>EffectsModel</name>
+    <message>
+        <source>None</source>
+        <translation>Ninguno</translation>
+    </message>
+    <message>
+        <source>Aqua</source>
+        <translation>Agua</translation>
+    </message>
+    <message>
+        <source>Blackboard</source>
+        <translation>Pizarra negra</translation>
+    </message>
+    <message>
+        <source>Grayscale</source>
+        <translation>Grises</translation>
+    </message>
+    <message>
+        <source>Negative</source>
+        <translation>Negativo</translation>
+    </message>
+    <message>
+        <source>Posterize</source>
+        <translation>Posterizar</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>Sepia</translation>
+    </message>
+    <message>
+        <source>Solarize</source>
+        <translation>Solarizar</translation>
+    </message>
+    <message>
+        <source>Whiteboard</source>
+        <translation>Pizarra blanca</translation>
+    </message>
+    <message>
+        <source>Emboss</source>
+        <translation>Relieve</translation>
+    </message>
+    <message>
+        <source>Sketch</source>
+        <translation>Boceto</translation>
+    </message>
+    <message>
+        <source>Neon</source>
+        <translation>Neón</translation>
+    </message>
+</context>
+<context>
+    <name>ExposureModel</name>
+    <message>
+        <source>Automatic Scene Detection</source>
+        <translation>Automático</translation>
+    </message>
+    <message>
+        <source>Augmented Reality</source>
+        <translation>Realidad aumentada</translation>
+    </message>
+    <message>
+        <source>Backlight</source>
+        <translation>Contraluz</translation>
+    </message>
+    <message>
+        <source>Barcode</source>
+        <translation>Código-barras</translation>
+    </message>
+    <message>
+        <source>Beach</source>
+        <translation>Playa</translation>
+    </message>
+    <message>
+        <source>Candlelight</source>
+        <translation>Vela</translation>
+    </message>
+    <message>
+        <source>Fireworks</source>
+        <translation>Fuegos artificiales</translation>
+    </message>
+    <message>
+        <source>Flowers</source>
+        <translation>Flores</translation>
+    </message>
+    <message>
+        <source>HDR</source>
+        <translation>HDR</translation>
+    </message>
+    <message>
+        <source>Landscape</source>
+        <translation>Paisaje</translation>
+    </message>
+    <message>
+        <source>Large Aperture</source>
+        <translation>Apertura amplia</translation>
+    </message>
+    <message>
+        <source>Night</source>
+        <translation>Noche</translation>
+    </message>
+    <message>
+        <source>Night Portrait</source>
+        <translation>Retrato nocturno</translation>
+    </message>
+    <message>
+        <source>Party</source>
+        <translation>Fiesta</translation>
+    </message>
+    <message>
+        <source>Portrait</source>
+        <translation>Retrato</translation>
+    </message>
+    <message>
+        <source>Small Aperture</source>
+        <translation>Apertura pequeña</translation>
+    </message>
+    <message>
+        <source>Snow</source>
+        <translation>Nieve</translation>
+    </message>
+    <message>
+        <source>Sports</source>
+        <translation>Deportes</translation>
+    </message>
+    <message>
+        <source>Spotlight</source>
+        <translation>Proyector</translation>
+    </message>
+    <message>
+        <source>Steady Photo</source>
+        <translation>Foto estable</translation>
+    </message>
+    <message>
+        <source>Sunset</source>
+        <translation>Puesta de sol</translation>
+    </message>
+    <message>
+        <source>Theatre</source>
+        <translation>Teatro</translation>
+    </message>
+    <message>
+        <source>Action</source>
+        <translation>Acción</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Apagado</translation>
+    </message>
+</context>
+<context>
+    <name>FlashModel</name>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Apagado</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>Encendido</translation>
+    </message>
+    <message>
+        <source>Red Eye Reduction</source>
+        <translation>Reducción ojos rojos</translation>
+    </message>
+    <message>
+        <source>Fill</source>
+        <translation>Relleno</translation>
+    </message>
+    <message>
+        <source>Torch</source>
+        <translation>Linterna</translation>
+    </message>
+    <message>
+        <source>Video Light</source>
+        <translation>Linterna vídeo</translation>
+    </message>
+    <message>
+        <source>Slow Sync Front Curtain</source>
+        <translation>Sinc. lenta en cortina frontal</translation>
+    </message>
+    <message>
+        <source>Slow Sync Rear Curtain</source>
+        <translation>Sinc. lenta en cortina trasera</translation>
+    </message>
+    <message>
+        <source>Manual</source>
+        <translation>Manual</translation>
+    </message>
+</context>
+<context>
+    <name>FocusModel</name>
+    <message>
+        <source>Manual</source>
+        <translation>Manual</translation>
+    </message>
+    <message>
+        <source>Hyperfocal</source>
+        <translation>Hiperfocal</translation>
+    </message>
+    <message>
+        <source>Infinity</source>
+        <translation>Infinito</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <source>Continuous</source>
+        <translation>Continuo</translation>
+    </message>
+    <message>
+        <source>Macro</source>
+        <translation>Macro</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
+    </message>
+</context>
+<context>
+    <name>GalleryUI</name>
+    <message>
+        <source>Deleting</source>
+        <translation>Borrando</translation>
+    </message>
+</context>
+<context>
+    <name>IsoModel</name>
+    <message>
+        <source>Auto ISO</source>
+        <translation>ISO Auto</translation>
+    </message>
+</context>
+<context>
+    <name>WbModel</name>
+    <message>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <source>Manual</source>
+        <translation>Manual</translation>
+    </message>
+    <message>
+        <source>Sunlight</source>
+        <translation>Soleado</translation>
+    </message>
+    <message>
+        <source>Cloudy</source>
+        <translation>Nublado</translation>
+    </message>
+    <message>
+        <source>Shade</source>
+        <translation>Sombra</translation>
+    </message>
+    <message>
+        <source>Tungsten</source>
+        <translation>Tungsteno</translation>
+    </message>
+    <message>
+        <source>Flash</source>
+        <translation>Flash</translation>
+    </message>
+    <message>
+        <source>Sunset</source>
+        <translation>Puesta de sol</translation>
+    </message>
+    <message>
+        <source>Warm Fluorescent</source>
+        <translation>Fluorescente cálido</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
+    </message>
+    <message>
+        <source>Fluorescent</source>
+        <translation>Fluorescente</translation>
+    </message>
+</context>
+</TS>
