0c0f788d18bb8e7680c511dae7ea5e27e2aeda60
ReleaseRoll <55141950+ReleaseRoll@users.noreply.github.com>
2019-10-04 06:37:38 +0800

    Update harbour-advanced-camera-zh_CN.ts

--- translations/harbour-advanced-camera-zh_CN.ts
+++ translations/harbour-advanced-camera-zh_CN.ts
@@ -240,7 +240,7 @@
     <name>GalleryUI</name>
     <message>
         <source>Deleting %1</source>
-        <translation type="unfinished"></translation>
+        <translation>正在删除 %1</translation>
     </message>
 </context>
 <context>
