70340ce4e183d610b1f0804926bdc46baf0db32f
Вячеслав Диконов <sdiconov@mail.ru>
2020-03-15 14:18:50 +0300

    Russian translation

---
+++ translations/harbour-advanced-camera-ru.ts
@@ -0,0 +1,330 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Advanced Camera</source>
+        <translation>Улучшенная камера</translation>
+    </message>
+</context>
+<context>
+    <name>EffectsModel</name>
+    <message>
+        <source>None</source>
+        <translation>Нет</translation>
+    </message>
+    <message>
+        <source>Aqua</source>
+        <translation>Под водой</translation>
+    </message>
+    <message>
+        <source>Blackboard</source>
+        <translation>Черная доска</translation>
+    </message>
+    <message>
+        <source>Grayscale</source>
+        <translation>Черно-белый</translation>
+    </message>
+    <message>
+        <source>Negative</source>
+        <translation>Негатив</translation>
+    </message>
+    <message>
+        <source>Posterize</source>
+        <translation>Постеризация</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>Сепия</translation>
+    </message>
+    <message>
+        <source>Solarize</source>
+        <translation>Соляризация</translation>
+    </message>
+    <message>
+        <source>Whiteboard</source>
+        <translation>Белая доска</translation>
+    </message>
+    <message>
+        <source>Emboss</source>
+        <translation>Рельеф</translation>
+    </message>
+    <message>
+        <source>Sketch</source>
+        <translation>Рисунок</translation>
+    </message>
+    <message>
+        <source>Neon</source>
+        <translation>Неоновое свечение</translation>
+    </message>
+</context>
+<context>
+    <name>ExposureModel</name>
+    <message>
+        <source>Automatic Scene Detection</source>
+        <translation>Обнаружение сцены</translation>
+    </message>
+    <message>
+        <source>Augmented Reality</source>
+        <translation>Дополненная реальность</translation>
+    </message>
+    <message>
+        <source>Backlight</source>
+        <translation>Задний Свет</translation>
+    </message>
+    <message>
+        <source>Barcode</source>
+        <translation>Штрихкод</translation>
+    </message>
+    <message>
+        <source>Beach</source>
+        <translation>Пляж</translation>
+    </message>
+    <message>
+        <source>Candlelight</source>
+        <translation>При свечах</translation>
+    </message>
+    <message>
+        <source>Fireworks</source>
+        <translation>Салют</translation>
+    </message>
+    <message>
+        <source>Flowers</source>
+        <translation>Цветы</translation>
+    </message>
+    <message>
+        <source>HDR</source>
+        <translation>HDR</translation>
+    </message>
+    <message>
+        <source>Landscape</source>
+        <translation>Пейзаж</translation>
+    </message>
+    <message>
+        <source>Large Aperture</source>
+        <translation>Большая апертура</translation>
+    </message>
+    <message>
+        <source>Night</source>
+        <translation>Ночь</translation>
+    </message>
+    <message>
+        <source>Night Portrait</source>
+        <translation>Ночной портрет</translation>
+    </message>
+    <message>
+        <source>Party</source>
+        <translation>Вечеринка</translation>
+    </message>
+    <message>
+        <source>Portrait</source>
+        <translation>Портрет</translation>
+    </message>
+    <message>
+        <source>Small Aperture</source>
+        <translation>Малая апертура</translation>
+    </message>
+    <message>
+        <source>Snow</source>
+        <translation>Снег</translation>
+    </message>
+    <message>
+        <source>Sports</source>
+        <translation>Спорт</translation>
+    </message>
+    <message>
+        <source>Spotlight</source>
+        <translation>Прожектор</translation>
+    </message>
+    <message>
+        <source>Steady Photo</source>
+        <translation>Неподвижный предмет</translation>
+    </message>
+    <message>
+        <source>Sunset</source>
+        <translation>Закат</translation>
+    </message>
+    <message>
+        <source>Theatre</source>
+        <translation>Театр</translation>
+    </message>
+    <message>
+        <source>Action</source>
+        <translation>Движение</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Выкл.</translation>
+    </message>
+</context>
+<context>
+    <name>FlashModel</name>
+    <message>
+        <source>Unknown</source>
+        <translation>Неизвестно</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Автоматически</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Выкл.</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>Вкл.</translation>
+    </message>
+    <message>
+        <source>Red Eye Reduction</source>
+        <translation>Убрать эффект красных глаз</translation>
+    </message>
+    <message>
+        <source>Fill</source>
+        <translation>Равномерный свет</translation>
+    </message>
+    <message>
+        <source>Torch</source>
+        <translation>Факел</translation>
+    </message>
+    <message>
+        <source>Video Light</source>
+        <translation>Видеосвет</translation>
+    </message>
+    <message>
+        <source>Slow Sync Front Curtain</source>
+        <translation>Замедленная синхронизация по передней шторке</translation>
+    </message>
+    <message>
+        <source>Slow Sync Rear Curtain</source>
+        <translation>Замедленная синхронизация по задней шторке</translation>
+    </message>
+    <message>
+        <source>Manual</source>
+        <translation>Вручную</translation>
+    </message>
+</context>
+<context>
+    <name>FocusModel</name>
+    <message>
+        <source>Manual</source>
+        <translation>Ручной</translation>
+    </message>
+    <message>
+        <source>Hyperfocal</source>
+        <translation>Гиперфокальный</translation>
+    </message>
+    <message>
+        <source>Infinity</source>
+        <translation>Бесконечность</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Автоматический</translation>
+    </message>
+    <message>
+        <source>Continuous</source>
+        <translation>Постоянный</translation>
+    </message>
+    <message>
+        <source>Macro</source>
+        <translation>Макро</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Неизвестно</translation>
+    </message>
+</context>
+<context>
+    <name>GalleryUI</name>
+    <message>
+        <source>Deleting %1</source>
+        <translation>Удаление %1</translation>
+    </message>
+</context>
+<context>
+    <name>IsoModel</name>
+    <message>
+        <source>Auto ISO</source>
+        <translation>Авто ISO</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsOverlay</name>
+    <message>
+        <source>Swap zoom controls</source>
+        <translation>Поменять управление приближением</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>нет</translation>
+    </message>
+    <message>
+        <source>Thirds</source>
+        <translation>по третям</translation>
+    </message>
+    <message>
+        <source>Ambience</source>
+        <translation>по краям</translation>
+    </message>
+    <message>
+        <source>Grid:</source>
+        <translation>Сетка:</translation>
+    </message>
+</context>
+<context>
+    <name>StorageModel</name>
+    <message>
+        <source>Internal storage</source>
+        <translation>Встроенная память</translation>
+    </message>
+</context>
+<context>
+    <name>WbModel</name>
+    <message>
+        <source>Auto</source>
+        <translation>Авто</translation>
+    </message>
+    <message>
+        <source>Manual</source>
+        <translation>Вручную</translation>
+    </message>
+    <message>
+        <source>Sunlight</source>
+        <translation>Солнце</translation>
+    </message>
+    <message>
+        <source>Cloudy</source>
+        <translation>Пасмурно</translation>
+    </message>
+    <message>
+        <source>Shade</source>
+        <translation>Тень</translation>
+    </message>
+    <message>
+        <source>Tungsten</source>
+        <translation>Лампа накаливания</translation>
+    </message>
+    <message>
+        <source>Flash</source>
+        <translation>Вспышка</translation>
+    </message>
+    <message>
+        <source>Sunset</source>
+        <translation>Закат</translation>
+    </message>
+    <message>
+        <source>Warm Fluorescent</source>
+        <translation>Флуорисцентная теплая лампа</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Неизвестно</translation>
+    </message>
+    <message>
+        <source>Fluorescent</source>
+        <translation>Флуорисцентная лампа</translation>
+    </message>
+</context>
+</TS>
